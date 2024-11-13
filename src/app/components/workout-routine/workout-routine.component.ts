8ccf1bb202526b71a503d7d70bb33760c6979743
Bernardo Marques <bernardohmarques@gmail.com>
2024-11-12 22:24:33 -0300

    chore: create workout component

--- src/app/components/workout-routine/workout-routine.component.ts
+++ src/app/components/workout-routine/workout-routine.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-workout-routine',
   templateUrl: './workout-routine.component.html',
   styleUrls: ['./workout-routine.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, MatExpansionModule],
 })
 export class WorkoutRoutineComponent implements OnInit {
   constructor() {}
